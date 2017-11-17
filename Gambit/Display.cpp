057c21f0fd4af504bfb04b5d75f4809e0d25adab
Dan <816.dan@gmail.com>
2017-11-17 11:14:27 -0700

    Small update to the display aspect. Have more plans for it in the future.

--- Gambit/Display.cpp
+++ Gambit/Display.cpp
@@ -6,18 +6,21 @@
 
 bool Display::DrawBoard(Board *board) {
     int index = 7;
+    char file = 'H';
 
     for (int j = 0; j < 8; ++j) {
+        std::cout << file-- << " ";
         for (int i = index; i > index - 8; --i) {
             if (board->bitBoard[i]) {
-                std::cout << 1;
+                std::cout << 1 << " ";
             } else {
-                std::cout << ".";
+                std::cout << ". ";
             }
         }
         index += 8;
         std::cout << std::endl;
     }
+    std::cout << "  1 2 3 4 5 6 7 8" << std::endl;
     std::cout << std::endl;
 
     return true;
